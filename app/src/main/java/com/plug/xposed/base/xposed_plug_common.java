059707dc5adffd5141e22dc5caa8500a4b2a2a95
袁莺 <5202570+xiabanyulen@user.noreply.gitee.com>
2021-08-13 22:28:47 +0800

    commit message

---
+++ app/src/main/java/com/plug/xposed/base/xposed_plug_common.java
@@ -0,0 +1,11 @@
+package com.plug.xposed.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class xposed_plug_common {
+    public static class plug_name {
+        static public String plug_name_sub_plug_loader = "sub_plug_loader";
+        static public String plug_name_pass_root = "pass_root";
+    }
+}
---
+++ app/src/main/java/com/units/config.java
@@ -0,0 +1,11 @@
+package com.units;
+
+import android.annotation.SuppressLint;
+
+public class config {
+
+    @SuppressLint("SdCardPath")
+    public static String plug_config_path = "/sdcard/plug_config.json";
+
+    public static String inject_jar_path;
+}
